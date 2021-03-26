12abe78d121409fc19cd3131ae829eb7ef8a8b4c
brandiw <brandimichelle.0911@gmail.com>
2021-03-26 13:31:39 -0700

    teal route added

--- index.js
+++ index.js
@@ -10,9 +10,23 @@ const app = express()
 // Declare routes that people can visit on the application
 app.get('/', function (req, res) {
     res.send(`
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+})
+
+app.get('/teal', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: teal;">Teal Page</h1>
+        </body>
     `)
 })
 
